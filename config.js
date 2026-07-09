c1b9ea3c0a4af2325c96996a06c65bf5a8ef13a8
fave-eng <fhendytheking@gmail.com>
2026-07-09 20:39:13 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+// Milena site config
+window.APP_CONFIG = {
+  STUDENT: 'milena',
+  STUDENT_NAME: 'Милена',
+  LEVEL: 'A2',
+  COURSE: 'Outcomes A2 2nd edition',
+  SB_URL: 'https://cbpvujrtyuepioqmrrqj.supabase.co',
+  SB_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNicHZ1anR5dWVwaW9xbXJyZWppIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM2MDY4NTMsImV4cCI6MjA5OTE4Mjg1M30.W_5ms3GRCRJhcww9zeYZU3MxcIE87pfQlHynzHoAb4U',
+  BOT_TOKEN: '8644812537:AAFVJit33e2RAoLMT4onuEBOw5ubUrPADJk',
+  CHAT_ID: '-1003981941831'
+};
